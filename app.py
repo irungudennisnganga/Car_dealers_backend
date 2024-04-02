c63d89e9c040d652c0769b19b27f5475939a2016
james <jleo56291@gmail.com>
2024-04-02 22:07:20 +0300

    feat ; add inventory "created post,patch,delete & get

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from models import User
+from models import User,Inventory
 from config import app,api,db,bcrypt
 from flask_restful import Resource
 from flask import request,jsonify,make_response
@@ -32,7 +32,7 @@ class SignupUser(Resource):
         first_name = data.get('first_name')
         last_name = data.get('last_name')
         image = data.get('image')
-        contact= data.gett('contact')
+        contact= data.get('contact')
         email = data.get('email')
         role =data.get('role')
         password = data.get('password')
@@ -40,7 +40,7 @@ class SignupUser(Resource):
         if not all([first_name, last_name, image, email,contact,role, password]):
             return make_response(jsonify({'errors': ['Missing required data']}), 400)
 
-        if User.query.filter_by(username=email).first():
+        if User.query.filter_by(email=email).first():
             return make_response(jsonify({'message': 'User already exists'}), 400)
 
         new_user = User(
@@ -89,11 +89,95 @@ class OneUser(Resource):
         )
         
         return response
+    
+# inventory
+
+class INVENTORY(Resource): 
+    # POST
+    def post(self):
+        data = request.json
+        new_inventory_item = Inventory(
+            make=data.get('make'),
+            image=data.get('image'),
+            price=data.get('price'),
+            currency=data.get('currency'),
+            model=data.get('model'),
+            year=data.get('year'),
+            VIN=data.get('VIN'),
+            color=data.get('color'),
+            mileage=data.get('mileage'),
+            body_style=data.get('body_style'),
+            transmission=data.get('transmission'),
+            fuel_type=data.get('fuel_type'),
+            engine_size=data.get('engine_size'),
+            drive_type=data.get('drive_type'),
+            trim_level=data.get('trim_level'),
+            gallery=data.get('gallery'),
+            condition=data.get('condition'),
+            availability=data.get('availability'),
+            cylinder=data.get('cylinder'),
+            doors=data.get('doors'),
+            features=data.get('features'),
+            stock_number=data.get('stock_number'),
+            purchase_cost=data.get('purchase_cost'),
+            profit=data.get('profit'),
+            user_id=data.get('user_id')
+        )
+        db.session.add(new_inventory_item)
+        db.session.commit()
+        return make_response (jsonify({'message': 'Inventory created successfully'}), 201)
+    
+    # GET
+    
+    def get(self,id=None):
+        if id:
+            inventory_item = Inventory.query.filter_by(id=id).first()
+            if inventory_item:
+                return make_response (jsonify(inventory_item.serializer()))
+            return {'message': 'Inventory item not found'}, 404
+        else:
+            items = Inventory.query.all()
+            return make_response (jsonify([item.serializer()for item in items]))
+        
+        # PATCH
+        
+
+class inventory_update(Resource):
+    def patch(self,id):
+        inventory_item = Inventory.query.filter_by(id=id).first()
+        if inventory_item:
+            data = request.json
+            for key, value in data.items():
+                if hasattr(inventory_item,key):
+                    setattr(inventory_item,key,value)
+
+                db.session.commit()
+                return {'message':"Inventory item updated successfully"},200
+            return {'meaage':'Inventory item not found'}, 404
+        
+        # DELETE
+        
+    def delete(self,id):
+        inventory_item = Inventory.query.filter_by(id=id).first()
+        if inventory_item:
+            db.session.delete(inventory_item)
+            db.session.commit()
+            
+            return{'message': 'Inventory item deleted successfully'}, 200
+        return {'message': "Inventory item not found"}, 404
+
+
+
+    
 
 api.add_resource(AllUsers, '/user')
 api.add_resource(OneUser, '/user/<int:id>')
 api.add_resource(Login, '/login')
 api.add_resource(SignupUser, '/signup')
+api.add_resource(inventory_update,"/inventory/<int:id>")
+api.add_resource(INVENTORY, '/inventory')
+
+
 
 
 if __name__ == "__main__":
